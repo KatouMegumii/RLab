9f8e7ea658ecb32f93f0f361b7779884581a2c5e
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2022-10-20 00:37:04 +0100

    add some codes on testing function

--- RLabTest.R
+++ RLabTest.R
@@ -1,9 +1,25 @@
 library(nycflights13)
 library(tidyverse)
 
-filter(flights,month==1,day==1)
+test1=filter(flights, month == 11 | month == 12)
+test2=filter(flights,month==11,month==12)
+test3=filter(flights, month == 11&month == 12)
+
+test4=filter(flights,month==1,day==1)
+test5=filter(flights,month==1&day==1)
 
 df <- tibble(x = c(1, NA, 3))
+print(df)
 
 filter(df, x > 1)
 filter(df, is.na(x) | x > 1)
+
+round(4.256,1)
+ceiling(4.25)
+floor(4.25)
+
+1&0
+
+filter(flights,is.na(dep_time))
+
+NA * 0
