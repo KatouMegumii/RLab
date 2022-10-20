fb5742ff7c8b2b7079b276567d0c9bb1461efbd8
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2022-10-20 21:09:35 +0100

    test function arrange and select

--- RLabTest.R
+++ RLabTest.R
@@ -1,12 +1,13 @@
+library(dplyr)
 library(nycflights13)
 library(tidyverse)
 
-test1=filter(flights, month == 11 | month == 12)
-test2=filter(flights,month==11,month==12)
-test3=filter(flights, month == 11&month == 12)
+filter(flights, month == 11 | month == 12)
+filter(flights,month==11,month==12)
+filter(flights, month == 11&month == 12)
 
-test4=filter(flights,month==1,day==1)
-test5=filter(flights,month==1&day==1)
+filter(flights,month==1,day==1)
+filter(flights,month==1&day==1)
 
 df <- tibble(x = c(1, NA, 3))
 print(df)
@@ -23,3 +24,16 @@ floor(4.25)
 filter(flights,is.na(dep_time))
 
 NA * 0
+
+by_group=group_by(flights,dest)
+test1=arrange(by_group,year,month,day,.by_group = TRUE)
+head(test1)
+test2=arrange(by_group,year,month,day,.by_group = FALSE)
+head(test2)
+
+data=data.frame(x=c(1,2,NA))
+arrange(data,desc(is.na(x)))
+arrange(data,desc(is.na(x)),x)
+
+flights[c("month","year")]
+select(flights,month,year)
