7354f450611231ed81e32455d8b047fa491e6dff
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2022-10-27 16:09:03 +0100

    lab5 codes

---
+++ Lab5.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(MASS)
+library(car)
+load("ESS_data2.Rdata")
+
+cor.test(data$trstun, data$trstprl, use = "complete.obs")
+
+ggplot(data, aes(trstprl, trstun)) + geom_point(alpha = .1, shape = 1, position = position_jitter(width = 1, height = .5))
+
+ggplot(data, aes(trstprl, trstun)) + geom_point(alpha = .1, shape = 1, position = position_jitter(width = 1, height = .5)) + geom_smooth(method = lm, se = FALSE, size = 2)
+
+dplyr::select(data,imbgeco)
+
+table(data$imbgeco)
+attributes(data$imbgeco)
+
+reg1 <- lm(trstprl ~ trstun, data = data)
+summary(reg1)
+
+lm()
