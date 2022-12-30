e55cff4351b5484aea133daaca1a64a5fd6c5466
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2022-12-30 17:38:05 +0000

    filter numeric variables

--- FinalEssay/FinalTask.R
+++ FinalEssay/FinalTask.R
@@ -1,3 +1,15 @@
 data=readRDS("FinalEssay/ESS9.rds")
-attributes(data$vote)
-datavote=data$vote
+#筛选离散变量
+datanum=data.frame(1:49519)
+col=vector()
+i=1
+while (i <= 7900){
+  data_temp=data[,i]
+  if (is.numeric(data_temp) == TRUE){
+    col=append(col,i)
+  }
+}
+
+cor.test()
+
+
