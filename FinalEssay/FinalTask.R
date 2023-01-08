6b8a78f34e5b3c61990af2aa2b0ad211c4ff11b5
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2023-01-08 15:56:13 +0100

    codes fixed

--- FinalEssay/FinalTask.R
+++ FinalEssay/FinalTask.R
@@ -1,15 +1,2 @@
 data=readRDS("FinalEssay/ESS9.rds")
-#筛选离散变量
-datanum=data.frame(1:49519)
-col=vector()
-i=1
-while (i <= 7900){
-  data_temp=data[,i]
-  if (is.numeric(data_temp) == TRUE){
-    col=append(col,i)
-  }
-}
-
-cor.test()
-
 
